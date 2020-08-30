88a1e97f7092caa97f27de4de9a5d2e28de613d5
James Cartledge <jcartledge@gmail.com>
2020-08-30 19:02:12 +1000

    Start to set up server.

---
+++ server/src/schema.ts
@@ -0,0 +1,35 @@
+import { gql } from "apollo-server";
+
+export const typeDefs = gql`
+  type DiaryEntry {
+    risk: String
+    date: String
+    whatHappened: String
+    wentWell: String
+    couldBeImproved: String
+    notWell: String
+  }
+
+  input DiaryEntryInput {
+    risk: String
+    date: String
+    whatHappened: String
+    wentWell: String
+    couldBeImproved: String
+    notWell: String
+  }
+
+  type Query {
+    diaryEntry(date: String!): DiaryEntry
+  }
+
+  type Mutation {
+    updateDiaryEntry(diaryEntry: DiaryEntryInput!): UpdateDiaryEntryResponse
+  }
+
+  type UpdateDiaryEntryResponse {
+    success: Boolean!
+    message: String
+    diaryEntry: DiaryEntry
+  }
+`;
