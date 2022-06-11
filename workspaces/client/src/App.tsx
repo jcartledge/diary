afcbf969da588b2dcbf3a16422d35ee4adf54098
James Cartledge <jcartledge@gmail.com>
2022-06-11 19:04:01 +1000

    Strict mode

--- workspaces/client/src/App.tsx
+++ workspaces/client/src/App.tsx
@@ -1,22 +1,24 @@
 import { Auth0Provider } from "@auth0/auth0-react";
-import React from "react";
+import React, { StrictMode } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { LocaleContext } from "./context/LocaleContext";
 import { AppRoutes } from "./routes";
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Auth0Provider
-        domain={process.env.REACT_APP_AUTH0_DOMAIN ?? ""}
-        clientId={process.env.REACT_APP_AUTH0_CLIENT_ID ?? ""}
-        redirectUri={window.location.origin}
-      >
-        <LocaleContext.Provider value={navigator.language}>
-          <AppRoutes />
-        </LocaleContext.Provider>
-      </Auth0Provider>
-    </BrowserRouter>
+    <StrictMode>
+      <BrowserRouter>
+        <Auth0Provider
+          domain={process.env.REACT_APP_AUTH0_DOMAIN ?? ""}
+          clientId={process.env.REACT_APP_AUTH0_CLIENT_ID ?? ""}
+          redirectUri={window.location.origin}
+        >
+          <LocaleContext.Provider value={navigator.language}>
+            <AppRoutes />
+          </LocaleContext.Provider>
+        </Auth0Provider>
+      </BrowserRouter>
+    </StrictMode>
   );
 };
 
