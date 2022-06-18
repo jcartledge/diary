cec2ead1dd0069ee0e449dd2d8a17031cb76f3c3
James Cartledge <jcartledge@gmail.com>
2022-06-18 19:50:09 +1000

    Server coverage

--- workspaces/server/vitest.config.ts
+++ workspaces/server/vitest.config.ts
@@ -4,7 +4,14 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     coverage: {
+      all: true,
+      exclude: ["config.ts", "index.ts", "resolvers-types.ts"],
+      src: ["./src"],
       reportsDirectory: "./output/coverage/report",
+      lines: 100,
+      functions: 100,
+      branches: 100,
+      statements: 100,
     },
   },
 });
