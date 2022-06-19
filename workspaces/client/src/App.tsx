4b8c4e64f4483640a97a8deeb183a9ddd12b61dd
James Cartledge <jcartledge@gmail.com>
2022-06-20 00:17:29 +1000

    extract const

--- workspaces/client/src/App.tsx
+++ workspaces/client/src/App.tsx
@@ -6,11 +6,11 @@ import { client } from "./graphql/client";
 import { AppRoutes } from "./routes/AppRoutes";
 import { TogglesProvider } from "./toggles/TogglesProvider";
 
+const toggles = (process.env.REACT_APP_TOGGLES || "").split(/\s+/);
+
 const App: React.FC = () => {
   return (
-    <TogglesProvider
-      toggles={(process.env.REACT_APP_TOGGLES || "").split(/\s+/)}
-    >
+    <TogglesProvider toggles={toggles}>
       <ApolloProvider client={client}>
         <BrowserRouter>
           <LocaleContext.Provider value={navigator.language}>
