cddde035415e9b7257cff76a165c84d500d34cc1
James Cartledge <jcartledge@gmail.com>
2022-06-20 00:52:14 +1000

    coverage

--- workspaces/client/src/toggles/index.ts
+++
@@ -1,6 +0,0 @@
-import { Toggle } from "./Toggle";
-import { TogglesProvider } from "./TogglesProvider";
-import { useToggle } from "./useToggle";
-import { useToggles } from "./useToggles";
-
-export { useToggle, useToggles, Toggle, TogglesProvider };
