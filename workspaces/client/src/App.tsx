201a93c5a6ab3e7a8d049b4943da7824bf8d1e72
James Cartledge <jcartledge@gmail.com>
2022-06-19 19:49:09 +1000

    Remove unused dependency

--- workspaces/client/src/App.tsx
+++ workspaces/client/src/App.tsx
@@ -1,5 +1,4 @@
 import { ApolloProvider } from "@apollo/client";
-import { Auth0Provider } from "@auth0/auth0-react";
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { LocaleContext } from "./context/LocaleContext";
@@ -10,15 +9,9 @@ const App: React.FC = () => {
   return (
     <ApolloProvider client={client}>
       <BrowserRouter>
-        <Auth0Provider
-          domain={process.env.REACT_APP_AUTH0_DOMAIN ?? ""}
-          clientId={process.env.REACT_APP_AUTH0_CLIENT_ID ?? ""}
-          redirectUri={window.location.origin}
-        >
-          <LocaleContext.Provider value={navigator.language}>
-            <AppRoutes />
-          </LocaleContext.Provider>
-        </Auth0Provider>
+        <LocaleContext.Provider value={navigator.language}>
+          <AppRoutes />
+        </LocaleContext.Provider>
       </BrowserRouter>
     </ApolloProvider>
   );
