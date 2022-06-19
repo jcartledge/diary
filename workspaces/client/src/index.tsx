11206c9ce49cfc17725c50fe076be61deb0c26b2
James Cartledge <jcartledge@gmail.com>
2022-06-19 19:51:57 +1000

    Remove serviceworker

--- workspaces/client/src/index.tsx
+++ workspaces/client/src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 import "./tailwind.css";
 
 const container = document.getElementById("root");
@@ -11,5 +10,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-serviceWorker.register();
