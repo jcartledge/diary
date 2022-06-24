0777779e81d3cafe30ac8d4857aef36d09950f02
James Cartledge <jcartledge@gmail.com>
2022-06-24 22:01:48 +1000

    Add auth0 back

--- workspaces/client/src/App.tsx
+++ workspaces/client/src/App.tsx
@@ -1,4 +1,5 @@
 import { ApolloProvider } from "@apollo/client";
+import { Auth0Provider } from "@auth0/auth0-react";
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { LocaleContext } from "./context/LocaleContext";
@@ -11,13 +12,19 @@ const toggles = (process.env.REACT_APP_TOGGLES || "").split(/\s+/);
 const App: React.FC = () => {
   return (
     <TogglesProvider toggles={toggles}>
-      <ApolloProvider client={client}>
-        <BrowserRouter>
-          <LocaleContext.Provider value={navigator.language}>
-            <AppRoutes />
-          </LocaleContext.Provider>
-        </BrowserRouter>
-      </ApolloProvider>
+      <Auth0Provider
+        domain={process.env.REACT_APP_AUTH0_DOMAIN ?? ""}
+        clientId={process.env.REACT_APP_AUTH0_CLIENT_ID ?? ""}
+        redirectUri={window.location.origin}
+      >
+        <ApolloProvider client={client}>
+          <BrowserRouter>
+            <LocaleContext.Provider value={navigator.language}>
+              <AppRoutes />
+            </LocaleContext.Provider>
+          </BrowserRouter>
+        </ApolloProvider>
+      </Auth0Provider>
     </TogglesProvider>
   );
 };
