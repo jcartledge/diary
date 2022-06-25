c6492ebed232fd78b728667ee404ac80acc712d4
James Cartledge <jcartledge@gmail.com>
2022-06-25 18:58:17 +1000

    move refactor

--- workspaces/client/src/test/wrappers/withAuth0Wrapper.tsx
+++ workspaces/client/src/test/wrappers/withAuth0Wrapper.tsx
@@ -1,6 +1,26 @@
 import { Auth0Context, Auth0ContextInterface } from "@auth0/auth0-react";
 import { createHelper } from "souvlaki";
-import { buildMockAuth0ContextValue } from "../buildMockAuth0ContextValue";
+import { vi } from "vitest";
+import { Builder } from "../../util/builder.types";
+
+const buildMockAuth0ContextValue: Builder<Auth0ContextInterface> = (
+  overrides = {}
+) => ({
+  buildAuthorizeUrl: vi.fn(),
+  isAuthenticated: true,
+  user: {},
+  isLoading: false,
+  buildLogoutUrl: vi.fn(),
+  getAccessTokenSilently:
+    vi.fn() as unknown as Auth0ContextInterface["getAccessTokenSilently"],
+  getAccessTokenWithPopup: vi.fn(),
+  getIdTokenClaims: vi.fn(),
+  loginWithRedirect: vi.fn(),
+  loginWithPopup: vi.fn(),
+  logout: vi.fn(),
+  handleRedirectCallback: vi.fn(),
+  ...overrides,
+});
 
 export const withAuth0Wrapper = createHelper(
   (
