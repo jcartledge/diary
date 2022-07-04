95bf2a6bdf8784922f7c3fddbb6217f9a5c9fdb3
James Cartledge <jcartledge@gmail.com>
2022-07-04 11:32:39 +1000

    navigation polyfill for tests

--- workspaces/client/src/lib/router/NavigationRouter.tsx
+++ workspaces/client/src/lib/router/NavigationRouter.tsx
@@ -1,9 +1,20 @@
-import { useState } from "react";
+import { type Navigation } from "@virtualstate/navigation";
+import { useEffect, useState } from "react";
 import { Router } from "./Router";
 
-export const NavigationRouter: React.FC<React.PropsWithChildren> = ({
+type NavigationRouterProps = React.PropsWithChildren<{
+  navigation: Navigation;
+}>;
+
+export const NavigationRouter: React.FC<NavigationRouterProps> = ({
+  navigation,
   children,
 }) => {
   const [path] = useState(window.location.pathname);
+  useEffect(() => {
+    const navigateListener = () => {};
+    navigation.addEventListener("navigate", navigateListener);
+    return () => navigation.removeEventListener("navigate", navigateListener);
+  }, []);
   return <Router initialPath={path}>{children}</Router>;
 };
