23cfe04c87ee9bb798a650d39a450ff5a0604aec
James Cartledge <jcartledge@gmail.com>
2022-07-09 20:05:08 +1000

    switch out tailwind

--- workspaces/client/src/index.tsx
+++ workspaces/client/src/index.tsx
@@ -1,7 +1,7 @@
+import "@picocss/pico";
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./tailwind.css";
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
