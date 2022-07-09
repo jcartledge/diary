66e87677d0e63454fcfbcc9cf2d3d12e95d4672e
James Cartledge <jcartledge@gmail.com>
2022-07-09 20:51:17 +1000

    de-cell

--- workspaces/client/src/app/components/organisms/DiaryHeader.tsx
+++ workspaces/client/src/app/components/organisms/DiaryHeader.tsx
@@ -1,5 +1,3 @@
-import Cell from "app/components/atoms/Cell";
-import { H1 } from "app/components/atoms/styled";
 import DateControl from "app/components/molecules/DateControl";
 import { DiaryEntryContext } from "app/context/diaryEntry/DiaryEntryContext";
 import React, { useContext } from "react";
@@ -7,14 +5,12 @@ import React, { useContext } from "react";
 const DiaryHeader: React.FC = () => {
   const { isDirty } = useContext(DiaryEntryContext);
   return (
-    <div className="grid md:grid-cols-2 lg:grid-cols-3">
-      <Cell className="text-center md:col-span-2 lg:col-span-3">
-        <H1>Diary</H1>
-        <div className={isDirty ? "italic" : "not-italic"}>
-          <DateControl />
-        </div>
-      </Cell>
-    </div>
+    <header>
+      <h1>Diary</h1>
+      <div className={isDirty ? "italic" : "not-italic"}>
+        <DateControl />
+      </div>
+    </header>
   );
 };
 
