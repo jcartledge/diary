17d0af02dd0db03480886badb55d634b491951ba
James Cartledge <jcartledge@gmail.com>
2022-07-09 20:51:57 +1000

    cleanup tailwind classes

--- workspaces/client/src/app/components/molecules/DateControl.tsx
+++ workspaces/client/src/app/components/molecules/DateControl.tsx
@@ -4,17 +4,11 @@ import { FormattedDate } from "app/components/atoms/FormattedDate";
 import React from "react";
 
 const DateControl: React.FC = () => (
-  <div className="flex">
-    <div className="flex-none">
-      <DatePrevButton />
-    </div>
-    <div className="flex-grow">
-      <FormattedDate />
-    </div>
-    <div className="flex-none">
-      <DateNextButton />
-    </div>
-  </div>
+  <>
+    <DatePrevButton />
+    <FormattedDate />
+    <DateNextButton />
+  </>
 );
 
 export default DateControl;
