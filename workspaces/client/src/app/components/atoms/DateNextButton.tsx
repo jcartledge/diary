615927492b24c030b78e0ba72d91b2a18a2f8e0f
James Cartledge <jcartledge@gmail.com>
2022-07-12 08:52:50 +1000

    Simplification

--- workspaces/client/src/app/components/atoms/DateNextButton.tsx
+++ workspaces/client/src/app/components/atoms/DateNextButton.tsx
@@ -1,27 +1,13 @@
 import { useDate } from "app/context/date/DateContext";
 import { Link } from "lib/router";
-import { DiaryDate } from "lib/util/date";
 
 const DateNextButton: React.FC = () => {
   const date = useDate();
   return date.isToday() ? (
-    <DateNextButtonDisabled />
+    <span aria-disabled>next</span>
   ) : (
-    <DateNextLink date={date} />
+    <Link to={`/page/${date.getNext().getKey()}`}>next</Link>
   );
-};
-
-const DateNextButtonDisabled: React.FC = () => (
-  <span className="secondary outline" aria-disabled>
-    next
-  </span>
-);
-
-type DateNextLinkProps = { date: DiaryDate };
-
-const DateNextLink: React.FC<DateNextLinkProps> = ({ date }) => {
-  const dateNextPath = `/page/${date.getNext().getKey()}`;
-  return <Link to={dateNextPath}>next</Link>;
 };
 
 export default DateNextButton;
