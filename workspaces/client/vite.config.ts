a1659d901cab0d7a2b3c45b15e7e7efddb63b1e3
James Cartledge <jcartledge@gmail.com>
2022-07-19 12:41:35 +1000

    Uplift env var handling for vite 3

--- workspaces/client/vite.config.ts
+++ workspaces/client/vite.config.ts
@@ -1,11 +1,10 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import env from "vite-plugin-env-compatible";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   build: {
     outDir: "build",
   },
-  plugins: [env({ prefix: "REACT_APP_" }), react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths()],
 });
