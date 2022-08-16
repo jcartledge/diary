98abd263cbec434200bb0fe20083dc1ab11c195c
James Cartledge <jcartledge@gmail.com>
2022-08-16 17:06:22 +1000

    invalidate cache on update diary entry

--- workspaces/client/src/app/queries/useUpdateDiaryEntryMutation.tsx
+++ workspaces/client/src/app/queries/useUpdateDiaryEntryMutation.tsx
@@ -1,18 +1,29 @@
 import { DiaryEntry } from "@diary/shared/types/DiaryEntry.types";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { bffUri } from "config";
 
-export const useUpdateDiaryEntryMutation = () =>
-  useMutation(async (diaryEntry: DiaryEntry) => {
-    const response = await fetch(`${bffUri}/diaryentry/${diaryEntry.date}`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ diaryEntry }),
-    });
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
+export const useUpdateDiaryEntryMutation = () => {
+  const queryClient = useQueryClient();
+  return useMutation(
+    async (diaryEntry: DiaryEntry) => {
+      const response = await fetch(`${bffUri}/diaryentry/${diaryEntry.date}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ diaryEntry }),
+      });
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return await response.json();
+    },
+    {
+      onSuccess: (_, diaryEntry) =>
+        queryClient.invalidateQueries([
+          "diaryEntry",
+          { isoDateString: diaryEntry.date },
+        ]),
     }
-    return await response.json();
-  });
+  );
+};
