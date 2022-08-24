c9d5ed7652b30a4d3eb52faa47c2b163ba3b8c08
James Cartledge <jcartledge@gmail.com>
2022-08-24 15:42:13 +1000

    Fix e2e

--- workspaces/client/src/config.ts
+++ workspaces/client/src/config.ts
@@ -2,7 +2,7 @@ const auth0 = {
   domain: import.meta.env.VITE_AUTH0_DOMAIN ?? "",
   clientId: import.meta.env.VITE_AUTH0_CLIENT_ID ?? "",
   redirectUri: import.meta.env.VITE_AUTH0_CALLBACK_URI ?? "",
-  audience: "http://diary-server",
+  audience: "https://diary-server",
   scope: "read:diary_entries_for_user write:diary_entries_for_user",
 };
 
