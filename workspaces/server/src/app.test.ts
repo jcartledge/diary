5e50dd24d60f203822dd7a3063d94e636d3f3631
James Cartledge <jcartledge@gmail.com>
2022-11-11 11:20:55 +1100

    remove unused test code

--- workspaces/server/src/app.test.ts
+++ workspaces/server/src/app.test.ts
@@ -1,26 +1,6 @@
-import { type JWKSMock } from "mock-jwks";
-import { getToken, startAuthServer } from "src/test/authHelper";
-import { AuthTestContext } from "src/test/AuthTestContext";
 import request from "supertest";
-import { afterAll, beforeAll, beforeEach, describe, expect, it } from "vitest";
+import { describe, expect, it } from "vitest";
 import { getAppWithRoutes } from "./app";
-
-let _jwksMockServer: JWKSMock;
-let _jwt: string;
-
-beforeAll(() => {
-  _jwksMockServer = startAuthServer();
-  _jwt = `Bearer ${getToken(_jwksMockServer)}`;
-});
-
-beforeEach<AuthTestContext>((context) => {
-  context.jwksMockServer = _jwksMockServer;
-  context.jwt = _jwt;
-});
-
-afterAll(() => {
-  _jwksMockServer.stop();
-});
 
 describe("app", () => {
   it("has a working healthcheck endpoint", async () => {
