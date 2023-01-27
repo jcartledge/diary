99c43dcc3695f71b10251fe0b9967615bebd0325
James Cartledge <jcartledge@gmail.com>
2023-01-27 21:42:27 +1100

    chore(deps): migrate auth0-react to 2

--- workspaces/client/src/config.ts
+++ workspaces/client/src/config.ts
@@ -1,9 +1,13 @@
-const auth0 = {
+import { Auth0ProviderOptions } from "@auth0/auth0-react";
+
+const auth0: Auth0ProviderOptions = {
   domain: import.meta.env.VITE_AUTH0_DOMAIN ?? "",
   clientId: import.meta.env.VITE_AUTH0_CLIENT_ID ?? "",
-  redirectUri: import.meta.env.VITE_AUTH0_CALLBACK_URI ?? "",
-  audience: "https://diary-server",
-  scope: "read:diary_entries_for_user write:diary_entries_for_user",
+  authorizationParams: {
+    redirect_uri: import.meta.env.VITE_AUTH0_CALLBACK_URI ?? "",
+    audience: "https://diary-server",
+    scope: "read:diary_entries_for_user write:diary_entries_for_user",
+  },
 };
 
 const toggles = (import.meta.env.VITE_TOGGLES || "").split(/\s+/);
