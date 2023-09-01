7b1c311fa71911c6371b9ee897c05e8e4f7fd2d3
James Cartledge <jcartledge@gmail.com>
2023-09-01 16:49:54 +1000

    chore: fix import

--- workspaces/client/src/test/setup.ts
+++ workspaces/client/src/test/setup.ts
@@ -1,17 +1,9 @@
 import { LocationMock } from "@jedmao/location";
-import matchers, {
-  TestingLibraryMatchers,
-} from "@testing-library/jest-dom/matchers";
+import '@testing-library/jest-dom/vitest'
 import { cleanup } from "@testing-library/react";
-import { afterAll, afterEach, beforeAll, beforeEach, expect } from "vitest";
+import { afterAll, afterEach, beforeAll, beforeEach } from "vitest";
 import "whatwg-fetch";
 import { server } from "./mocks/server";
-
-expect.extend(matchers);
-
-declare module "vitest" {
-  interface Assertion<T = any> extends TestingLibraryMatchers<T, void> {}
-}
 
 beforeEach(() => {
   window.location = new LocationMock("http://localhost/");
