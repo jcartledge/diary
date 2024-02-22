d285417e53b44de538ff24128e50760341115d36
James Cartledge <jcartledge@gmail.com>
2024-02-22 12:25:18 +1100

    chore: rename composeWrappers, handle empty case

--- workspaces/client/src/lib/util/composeWrappers.test.tsx
+++ workspaces/client/src/lib/util/composeWrappers.test.tsx
@@ -4,23 +4,26 @@ import { composeWrappers } from './composeWrappers';
 import { render, screen } from '@testing-library/react';
 
 describe('composeWrappers', () => {
+  it('renders content when no wrappers are provided to composeWrappers', () => {
+    render(<>test content</>, { wrapper: composeWrappers() });
+
+    expect(screen.getByText('test content')).not.toBe(null);
+  });
+
   it('returns the component when provided a single component', () => {
     const TestComponent: React.FC<React.PropsWithChildren> = ({ children }) => (<div data-testid="wrapper">{children}</div>);
-    const Composed = composeWrappers(TestComponent);
 
-    render(<Composed>some content</Composed>);
+    render(<>some content</>, { wrapper: composeWrappers(TestComponent) });
 
     expect(screen.getByTestId('wrapper')).toHaveTextContent('some content');
   });
 
-
   it('composes the components from outside in', () => {
     const OuterComponent: React.FC<React.PropsWithChildren> = ({ children }) => (<div data-testid="outer"><>[Outer]</> {children}</div>);
     const InnerComponent: React.FC<React.PropsWithChildren> = ({ children }) => (<><>[Inner]</> {children}</>);
-    const Composed = composeWrappers(OuterComponent, InnerComponent);
 
-    render(<Composed>[children]</Composed>);
+    render(<>[children]</>, { wrapper: composeWrappers(OuterComponent, InnerComponent) });
 
     expect(screen.getByTestId('outer')).toHaveTextContent('[Outer] [Inner] [children]');
-  })
+  });
 });
--- workspaces/client/src/lib/util/composeWrappers.tsx
+++ workspaces/client/src/lib/util/composeWrappers.tsx
@@ -1,10 +1,10 @@
 type ReactFCWithChildren = React.FC<React.PropsWithChildren>;
 
-export const composeWrappers = (...args: ReactFCWithChildren[]): ReactFCWithChildren => {
-  const [Outer, ...restArgs] = args;
-  return restArgs.length > 0 ?
-    wrap(Outer, composeWrappers(...restArgs)) :
-    Outer;
+export const composeWrappers = (...wrappers: ReactFCWithChildren[]): ReactFCWithChildren => {
+  const [Wrapper, ...restWrappers] = wrappers;
+  return restWrappers.length > 0 ?
+    wrap(Wrapper, composeWrappers(...restWrappers)) :
+    Wrapper;
 }
 
 const wrap = (Outer: ReactFCWithChildren, Inner: ReactFCWithChildren): ReactFCWithChildren => ({ children }) => (
