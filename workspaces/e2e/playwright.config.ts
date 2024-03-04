e24def6fb16370a98135ffc8062ac3f482d6fcdc
James Cartledge <jcartledge@gmail.com>
2024-03-04 11:57:03 +1100

    chore:update e2e config

--- workspaces/e2e/playwright.config.ts
+++ workspaces/e2e/playwright.config.ts
@@ -1,10 +1,10 @@
-const config = {
+import { defineConfig } from "@playwright/test";
+
+export default defineConfig({
   use: {
     ignoreHTTPSErrors: true,
     screenshot: "only-on-failure",
     video: "retain-on-failure",
     trace: "retain-on-failure",
   },
-};
-
-export default config;
+});
