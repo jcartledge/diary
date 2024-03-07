167b9f9f447c67bd3bd0257874f786a59db3c052
James Cartledge <jcartledge@gmail.com>
2024-03-08 10:44:15 +1100

    chore: attempt to fix e2e error

--- workspaces/e2e/playwright.config.ts
+++ workspaces/e2e/playwright.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
   use: {
-    ignoreHTTPSErrors: true,
     screenshot: "only-on-failure",
     video: "retain-on-failure",
     trace: "retain-on-failure",
