774cd4d98ae38b2d6cc4b2d8ae9d33dfa9da79d9
James Cartledge <jcartledge@gmail.com>
2024-05-12 11:45:48 +1000

    fix: async test

--- workspaces/client/src/app/components/atoms/FormattedDate.test.tsx
+++ workspaces/client/src/app/components/atoms/FormattedDate.test.tsx
@@ -1,5 +1,5 @@
 import { withResult } from "@diary/shared/ResultOrError";
-import { render } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import { wrapWithDate } from "app/context/date/DateContext.testWrapper";
 import { wrapWithLocale } from "app/context/locale/LocaleContext.testWrapper";
 import { DiaryDate } from "lib/util/DiaryDate";
@@ -8,18 +8,18 @@ import { describe, expect, it } from "vitest";
 import { FormattedDate } from "./FormattedDate";
 import { composeWrappers } from "lib/util/composeWrappers";
 
-describe("Diary", () => {
-  it("renders the date in the locale passed to it", () => {
+describe("FormattedDate", () => {
+  it("renders the date in the locale passed to it", async () => {
     const diaryDateResult = DiaryDate.from("2010-01-01");
     failWithError(diaryDateResult);
-    withResult(diaryDateResult, (date) => {
-      const formattedDate = render(<FormattedDate />, {
+    withResult(diaryDateResult, async (date) => {
+      render(<FormattedDate />, {
         wrapper: composeWrappers(wrapWithLocale("en-AU"), wrapWithDate(date)),
       });
 
-      expect(
-        formattedDate.queryByText(/Friday, 1 January 2010/),
-      ).toBeInTheDocument();
+      await waitFor(() => {
+        expect(screen.getByText(/Friday, 1 January 2010/)).toBeInTheDocument();
+      });
     });
   });
 });
